1362dbec5ca3a9255e8fada2d209866508f69229
CoderpreneurX <prabjeet.jhass@gmail.com>
2025-05-11 18:50:01 +0530

    Configured Alembic for Migrations

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,11 @@
-﻿
+﻿from pydantic_settings import BaseSettings
+from pathlib import Path
+import os
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,1 +1,4 @@
-﻿
+﻿from app.models import user, team, task, message  # import all models
+from sqlmodel import SQLModel
+
+SQLModelMeta = SQLModel
